d46dbc2f8149e9ac4b89a4864406100eca4d4add
CesarIzk <125629498+CesarIzk@users.noreply.github.com>
2026-05-18 16:15:35 -0600

    git db

--- socket-server/db.js
+++ socket-server/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 
 // 🔧 En Railway: configura estas variables en el panel → Variables
 const pool = mysql.createPool({
-    host:     process.env.DB_HOST     || "localhost",
+    host:     process.env.DB_HOST     || "mysql.railway.internal",
     user:     process.env.DB_USER     || "root",
-    password: process.env.DB_PASSWORD || "1234",
-    database: process.env.DB_NAME     || "socialmedia",
+    password: process.env.DB_PASSWORD || "BQxijSUPZBMtFEyOGnXISilxhXtRIDFs",
+    database: process.env.DB_NAME     || "railway",
     waitForConnections: true,
     connectionLimit: 10,
 });
